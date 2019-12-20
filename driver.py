ac2e15f4d31b61bb45ed5df37c269f2b993388a8
AustinEral <austineral@ymail.com>
2019-12-19 21:28:15 -0800

    added game loop

--- driver.py
+++ driver.py
@@ -1,9 +1,31 @@
-<<<<<<< HEAD
-import pygame as pg
-=======
+import pygame
+
+SCREEN_SIZE = (1000, 600)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+LOCATION = (500, 300)
+RADIUS = 100
+
 def main():
-    pass
+    pygame.init()
+
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+
+    running = True
+    while running:
+
+        # If window was closed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(WHITE)
+
+        pygame.draw.circle(screen, BLUE, LOCATION, RADIUS)
+
+        pygame.display.update()
+
 
 if __name__ == '__main__':
     main()
->>>>>>> 8179be9e3ded154a2bc62dcc0160175924da48fc
+
